b62d5b603b7cb61c9705a62bbf4666c10ce9f6e8
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-11-10 10:28:34 -0500

    imp(add book model, book service, and book json list)

---
+++ books/src/app/Model/book.model.ts
@@ -0,0 +1,9 @@
+export class Book {
+    constructor(
+        public id?: number,      
+        public title ?: string,  
+        public author?: string,
+        public editorial?: string,
+        public description?: string,
+    ) { }
+  }
---
+++ books/src/app/Services/book.service.ts
@@ -0,0 +1,19 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BookService {
+
+    constructor(private router: Router, private http: HttpClient) { }
+
+    public getAllBooks(url: string): Observable<any> {
+        return this.http.get(url);
+    }
+}
